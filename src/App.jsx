e3b54e530062dafb067bf7d08f212f71edcf1e7d
Rajveer Singh <work.rajveerbagga@gmail.com>
2026-05-03 09:00:00 +0530

    [Rajveer]:UC3:Displayed BridgeLabz logo using Property Binding

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import './App.css';
 
 function App() {
   let title = "Hello from BridgeLabz";
+  let logoUrl = "https://bridgelabz.com/wp-content/uploads/2021/08/BridgeLabz-Logo.png";
   
   return (
     <div className="app-container">
       <h1>{title}</h1>
+      <img src={logoUrl} alt="BridgeLabz Logo" />
     </div>
   );
 }
