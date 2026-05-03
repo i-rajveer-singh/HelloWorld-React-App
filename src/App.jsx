8fd7d0ff30f6a086af1973ebcf9c7eabcbf3cfde
Rajveer Singh <work.rajveerbagga@gmail.com>
2026-05-03 10:00:00 +0530

    [Rajveer]:UC4:Implemented click event to open BridgeLabz site

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import './App.css';
 function App() {
   let title = "Hello from BridgeLabz";
   let logoUrl = "https://bridgelabz.com/wp-content/uploads/2021/08/BridgeLabz-Logo.png";
+  let bridgeLabzUrl = "https://www.bridgelabz.com";
+
+  const onClickUrl = () => {
+    window.open(bridgeLabzUrl, "_blank");
+  };
   
   return (
     <div className="app-container">
       <h1>{title}</h1>
-      <img src={logoUrl} alt="BridgeLabz Logo" />
+      <img src={logoUrl} alt="BridgeLabz Logo" onClick={onClickUrl} style={{cursor: 'pointer'}} />
     </div>
   );
 }
