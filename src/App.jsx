f997675744913466599f8ae939f44c17681fbe67
Rajveer Singh <work.rajveerbagga@gmail.com>
2026-05-03 15:00:00 +0530

    [Rajveer]:UC9:Displayed HelloWorld using Home component with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
+import HelloWorld from './HelloWorld';
 import './App.css';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/helloworld" element={<HelloWorld />} />
     </Routes>
   );
 }
